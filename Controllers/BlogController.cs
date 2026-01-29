857ea4eef3abd17e06784a5829b911c6e01bdb88
Anchal-chaurasiya <anchalchaurasiya123@outlook.com>
2026-01-29 22:43:20 +0530

    commit

---
+++ Controllers/BlogController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MachineWeb.Controllers
+{
+    public class BlogController : Controller
+    {
+        public IActionResult Blog()
+        {
+            return View();
+        }
+    }
+}
